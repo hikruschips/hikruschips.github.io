faa69a0aa614d5b15ad1e871ee7bc967f3030f6b
Inoue Hikaru <hikruschips@gmail.com>
2019-08-07 15:42:34 +0900

    Update connection.php

--- chizu/connection.php
+++ chizu/connection.php
@@ -1,7 +1,7 @@
 
 <?php
 
-	require_once('/external_includes/mysql_pw.php');
+	require_once('mysql_pw.php');
 	
 		$conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		//set the PDO error mode to exception
